d32548b71b53287b1520cc310b8d03d429e9d180
kimberly@easypos.com.ph <kimberly@easypos.com.ph>
2022-07-20 16:18:46 +0800

    Display List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../hero";
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id:1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  // selected hero default none
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // once onSelect is triggered, we set the selectedHero to passed hero parameter
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
