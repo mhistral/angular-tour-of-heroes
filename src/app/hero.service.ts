894d9514b0d080b189327c9ceecf8cbe3a4ea77e
kimberly@easypos.com.ph <kimberly@easypos.com.ph>
2022-07-25 14:47:21 +0800

    Heroes and HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,34 +4,43 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-
-  /** 
+  /**
    * This is an example of a typical service-in-service scenario in which you inject the MessageService into the HeroService which is injected into the HeroesComponent.
-  */
-  constructor(private messageService: MessageService) { }
+   */
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  private heroesUrl = 'api/heroes'; // URL to web api
 
   /**
    * In this tutorial, HeroService.getHeroes() returns an Observable so that it can use the Angular HttpClient.get method to fetch the heroes and have HttpClient.get() return an Observable.
-   * @returns 
+   * @returns
    */
   getHeroes(): Observable<Hero[]> {
     // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
-    const heroes =  of(HEROES)
+    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes
+    return heroes;
   }
 
-  getHero(id: number): Observable<Hero>{
+  getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
+    const hero = HEROES.find((h) => h.id === id)!;
     this.messageService.add(`HeroService: Fetched hero id ${id}`);
     return of(hero);
   }
 
-  
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
